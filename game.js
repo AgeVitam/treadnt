aad1a934e6ccfc1dd988a541bc5e6839c1ed9ef9
agevitam <mastersebastian@gmail.com>
2017-02-03 19:26:10 -0500

    minor changes and adds cc0 license

--- game.js
+++ game.js
@@ -1,3 +1,17 @@
+/*
+Treadn't - a game based on a snake flag meme
+Written in 2017 by "AgeVitam" sebastian.naugahyde@gmail.com
+
+To the extent possible under law, the author(s) have dedicated all copyright 
+and related and neighboring rights to this software to the public domain 
+worldwide. This software is distributed without any warranty.
+
+You should have received a copy of the CC0 Public Domain Dedication along 
+with this software. 
+If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+*/
+
+
 //declarations
 var stage = document.getElementById("gameCanvas");
 stage.width = 640;
